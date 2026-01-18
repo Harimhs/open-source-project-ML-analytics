f96e94d20f396cd02de84d50fbb1b5700924ca07
Hariharasudhan M <harimhs028@gmail.com>
2026-01-18 21:36:12 +0530

    Add Fly.io config

--- app.py
+++ app.py
@@ -50,4 +50,7 @@ def predict():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
+
